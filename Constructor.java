5f802eab14e7e3d9012406278183f2a3102efcea
Vamsikrishnam12345 <106382210+Vamsikrishnam12345@users.noreply.github.com>
2022-06-16 16:16:03 +0530

    Add files via upload

---
+++ Constructor.java
@@ -0,0 +1,15 @@
+public class Constructor {
+    constructor(int a) {
+        System.out.println("constructor with parameter");
+    }
+    constructor( {
+    }
+    constructor(int a, sting name){
+        System.out.println()"different in parameter");
+    }
+    constructor(string name,int a){
+        System.out.println()"constructor is differ in the order of the parameters")
+
+    }
+    }
+}
