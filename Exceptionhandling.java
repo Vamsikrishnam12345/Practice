2468d55bbbeee815080daaeccef7a20d853efa09
Vamsikrishnam12345 <106382210+Vamsikrishnam12345@users.noreply.github.com>
2022-06-16 16:46:09 +0530

    Add files via upload

---
+++ Exceptionhandling.java
@@ -0,0 +1,13 @@
+public class Exceptionhandling {
+    package exceptionHandling;
+    public class ExceptionWithoutError
+    {
+        public static void main(String[] args)
+        {
+            System.out.println("java");
+            System.out.println("python");
+            System.out.println("mysql");
+            System.out.println("mongodb");
+        }
+    }
+}
